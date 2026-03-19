526e7d4db7caf1a6af352b0ad1566bb3e382ad55
Salem Almulaifi <mulaifi@gmail.com>
2026-03-19 04:06:47 +0300

    feat(i18n): self-host Inter and Tajawal fonts via next/font, remove Google Fonts CDN

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,6 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-        />
       </Head>
       <body>
         <Main />
